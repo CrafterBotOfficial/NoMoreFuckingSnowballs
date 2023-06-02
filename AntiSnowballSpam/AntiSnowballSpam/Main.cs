3bc22239a32547afefbbc4a17a7c92c4d2834304
Crafterbot <118089496+CrafterBotOfficial@users.noreply.github.com>
2023-06-02 19:30:01 -0400

    Initial commit

---
+++ AntiSnowballSpam/AntiSnowballSpam/Main.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace AntiSnowballSpam
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    internal class Main : BaseUnityPlugin
+    {
+        internal const string
+            GUID = "crafterbot.antisnowballspam",
+            NAME = "Anti-Snowball",
+            VERSION = "1.0.0";
+        internal static Main Instance;
+
+        internal bool RoomModded => GorillaNetworking.GorillaComputer.instance.currentGameMode.Contains("modded".ToUpper());
+        internal bool InForest;
+
+        internal Main()
+        {
+            Instance = this;
+            new Harmony(GUID).PatchAll();
+        }
+    }
+}
---
+++ AntiSnowballSpam/AntiSnowballSpam/Patches.cs
@@ -0,0 +1,22 @@
+﻿using GorillaNetworking;
+using HarmonyLib;
+
+namespace AntiSnowballSpam
+{
+    [HarmonyPatch]
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(GorillaGameManager), "LaunchSlingshotProjectile"), HarmonyPrefix]
+        private static void Hook_SnowBallThrown()
+        {
+            if (Main.Instance.RoomModded && Main.Instance.InForest)
+                return;
+        }
+
+        [HarmonyPatch(typeof(GorillaNetworkJoinTrigger), "OnBoxTriggered"), HarmonyPostfix]
+        private static void Hook_GeoOnTriggered(GorillaNetworkJoinTrigger __instance)
+        {
+            Main.Instance.InForest = __instance.gameModeName == "forest";
+        }
+    }
+}
